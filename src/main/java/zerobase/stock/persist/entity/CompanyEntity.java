a7463bfadd11b79776cf09e3f9c19301682ba6f2
parktj <ekswn120@gmail.com>
2023-10-30 20:34:14 +0900

    CompanyEntity, DividendEntity 생성

---
+++ src/main/java/zerobase/stock/persist/entity/CompanyEntity.java
@@ -0,0 +1,26 @@
+package zerobase.stock.persist.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "COMPANY")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ticker;
+
+    private String name;
+}
---
+++ src/main/java/zerobase/stock/persist/entity/DividendEntity.java
@@ -0,0 +1,29 @@
+package zerobase.stock.persist.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity(name = "DIVIDEND")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DividendEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long companyId;
+
+    private LocalDateTime date;
+
+    private String dividend;
+}
