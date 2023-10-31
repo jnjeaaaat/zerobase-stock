296bed0619add0f549ad0436369e5eb7588a61e2
parktj <ekswn120@gmail.com>
2023-10-31 21:10:05 +0900

    오전 12시마다 배당금 정보 받아오기 스케쥴링

--- src/main/java/zerobase/stock/StockApplication.java
+++ src/main/java/zerobase/stock/StockApplication.java
@@ -2,16 +2,20 @@ package zerobase.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import zerobase.stock.model.Company;
 import zerobase.stock.scraper.YahooFinanceScraper;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class, args);
+
+        System.out.println("Main -> " + Thread.currentThread().getName());
 
 //        YahooFinanceScraper yahooFinanceScraper = new YahooFinanceScraper();
 ////        var result = yahooFinanceScraper.scrap(Company.builder().ticker("O").build());
--- src/main/java/zerobase/stock/persist/entity/DividendEntity.java
+++ src/main/java/zerobase/stock/persist/entity/DividendEntity.java
@@ -3,16 +3,20 @@ package zerobase.stock.persist.entity;
 import lombok.*;
 import zerobase.stock.model.Dividend;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity(name = "DIVIDEND")
 @Getter
 @ToString
 @NoArgsConstructor
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = { "companyId", "date" }
+                )
+        }
+)
 public class DividendEntity {
 
     @Id
