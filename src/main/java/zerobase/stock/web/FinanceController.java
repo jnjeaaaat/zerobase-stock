0e14761bd90285fb08bc7772e81ba8daffc1e0b1
parktj <ekswn120@gmail.com>
2023-10-31 12:37:12 +0900

    회사 정보 & 배당금 리스트 조회

--- src/main/java/zerobase/stock/web/FinanceController.java
+++ src/main/java/zerobase/stock/web/FinanceController.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zerobase.stock.service.FinanceService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/finance")
 public class FinanceController {
 
+    private final FinanceService financeService;
+
     @GetMapping("/dividend/{companyName}")
     public ResponseEntity<?> searchFinance(@PathVariable String companyName) {
-        return null;
+        return ResponseEntity
+                .ok(this.financeService.getDividendByCompanyName(companyName));
     }
 
 }
